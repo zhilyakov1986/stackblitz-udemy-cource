5a7d8113ce8baeb54d5df3fea9572a60bb75b6b2
Andrey Zhilyakov <zhilyakov1986@gmail.com>
2023-09-25 08:54:28 -0400

    show form challenge

--- src/components/NewExpense/NewExpense.tsx
+++ src/components/NewExpense/NewExpense.tsx
@@ -1,15 +1,27 @@
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
+import { useState } from 'react';
 
 const NewExpense = (props) => {
+  const [showForm, setShowForm] = useState(false);
+  const handleNewExpenseClick = () => {
+    setShowForm(true);
+  };
+  
   const addedExpenseItem = (addedExpense) => {
     const id = Math.random();
     const expenseData = { ...addedExpense, id };
     props.onAddedExpense(expenseData);
+    setShowForm(false)
   };
+  const onCancel = () => {
+    setShowForm(false);
+  }
+  
   return (
     <div className="new-expense">
-      <ExpenseForm onSavedExpenseData={addedExpenseItem} />
+      {!showForm && <button onClick={handleNewExpenseClick}>Enter New Expense</button>}
+      {showForm && <ExpenseForm onSavedExpenseData={addedExpenseItem} handleCancel={onCancel}/>}
     </div>
   );
 };
