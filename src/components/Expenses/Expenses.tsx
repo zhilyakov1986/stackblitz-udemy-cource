e7671b1bb49bc75e6e1fa4cc91078903c2a99ac9
Andrey Zhilyakov <zhilyakov1986@gmail.com>
2023-09-25 06:19:52 -0400

    conditional return

--- src/components/Expenses/Expenses.tsx
+++ src/components/Expenses/Expenses.tsx
@@ -1,18 +1,18 @@
-import { useState } from 'react';
-import Card from '../UI/Card';
-import ExpensesFilter from './ExpensesFilter';
-import ExpenseItem from './ExpenseItem';
-import './Expenses.css';
+import { useState } from "react";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredYear, setFilteredYear] = useState("2020");
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
 
   const filteredExpenses = props.items.filter((expense) => {
-
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
@@ -23,17 +23,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-
-        <ul className="expenses-list">
-          {filteredExpenses.map((expense) => (
-            <ExpenseItem
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-          ))}
-        </ul>
+        <ExpensesList items={filteredExpenses}/>
       </Card>
     </div>
   );
